2a3827fd881d0ab780c8986477d1204e0af2e0e5
Kyle Coffey <mariorules24@gmail.com>
2021-12-18 03:07:09 -0600

    create main program

---
+++ Source/Minsk/Main.cpp
@@ -0,0 +1,27 @@
+#include <fmt/format.h>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    while (true)
+    {
+        fmt::print("> ");
+        std::string line;
+        if (!std::getline(std::cin, line))
+        {
+            fmt::print("\n");
+            break;
+        }
+
+        if (line == "1 + 2 * 3")
+        {
+            fmt::print("7\n");
+        }
+        else
+        {
+            fmt::print("ERROR: Invalid expression!\n");
+        }
+    }
+    return 0;
+}
